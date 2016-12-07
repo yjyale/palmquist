68dd9bd0e71a497680447b77f2f283b9d3bdc893
osmandin <osmandin@gmail.com>
2016-12-07 15:13:35 -0500

    Remove stub data from advanced item page.

--- src/main/java/edu/yale/controller/AdvancedController.java
+++ src/main/java/edu/yale/controller/AdvancedController.java
@@ -2,7 +2,6 @@ package edu.yale.controller;
 
 import edu.yale.domain.Person;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,19 +11,9 @@ import org.springframework.web.servlet.ModelAndView;
 public class AdvancedController {
 
     @RequestMapping(value = "/advanced", method = RequestMethod.GET)
-    public String greetingForm(@ModelAttribute Person person) {
-        ModelAndView modelAndView = new ModelAndView("persons");
-
-        // FIXME: remove the stub
-
-        Person person1 = new Person();
-        person1.setFullName("Nancy Ajram");
-        person1.setAlbumbox("10");
-        person1.setCities("Dubai");
-        person1.setNations("UAE");
-        modelAndView.addObject(person);
-
-        return "advanced";
+    public ModelAndView greetingForm(@ModelAttribute Person person) {
+        ModelAndView modelAndView = new ModelAndView("advanced");
+        return modelAndView;
     }
 }
 
--- src/main/java/edu/yale/controller/SingleItemController.java
+++ src/main/java/edu/yale/controller/SingleItemController.java
@@ -23,9 +23,18 @@ public class SingleItemController {
     }
 
     @RequestMapping(value = "/singleitem", method = RequestMethod.GET)
-    public String greetingForm(Model model,  @RequestParam(value = "id", required = true) Long id) {
-        Page<Person> persons = personService.findByPersonId(id, new PageRequest(0, 1));
-        Person person = persons.iterator().next();
+    public String greetingForm(Model model,  @RequestParam(value = "id", required = false) Long id, @RequestParam(value="fullName", required = false)  String fullName) {
+
+        Person person = new Person();
+
+        if (id != null) {
+            Page<Person> persons = personService.findByPersonId(id, new PageRequest(0, 1));
+            person = persons.iterator().next();
+        } else if (fullName != null) {
+            Page<Person> persons = personService.findByFullname(fullName, new PageRequest(0, 1));
+            person = persons.iterator().next();
+        }
+
         model.addAttribute("person", person);
         return "singleitem";
     }
